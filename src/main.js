d6557fcbd755a65aa86e01085573fc873de5f9de
onerunsall <413038044@qq.com>
2020-03-20 15:06:00 +0800

    pack

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import moment from 'moment'
 import qs from 'qs';
 import h5p from '../build/h5p.js'
  
-debugger 
+debugger  
 Vue.prototype.$version = '2003201439-29f3dd9'
 Vue.config.productionTip = false
 
