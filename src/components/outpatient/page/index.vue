732d2d09963c5edfab7cdb5f28c0e77635a36968
BlockHead <1955372313@qq.com>
2020-03-20 09:30:06 +0800

    sss

--- src/components/outpatient/page/index.vue
+++ src/components/outpatient/page/index.vue
@@ -286,15 +286,15 @@ console.log(this.scrollTop)
     this.$axios.post('/c2/patient/itemadd',qs.stringify({
 			clinicId : this.$store.state.outpatientEntrance.loginRefresh().clinic.clinicId,
 			hospitalId : this.$store.state.outpatientEntrance.loginRefresh().hospital.hospitalId,
-    		password : this.$store.state.outpatientEntrance.loginRefresh().password,
-    		realname : this.$store.state.outpatientEntrance.loginRefresh().realname,
-    		tel	:  this.$store.state.outpatientEntrance.loginRefresh().tel,
-    		remark : this.$store.state.outpatientEntrance.loginRefresh().remark,
-    		idcardNo : this.$store.state.outpatientEntrance.loginRefresh().idcardNo
+    		password : this.account.password,
+    		realname : this.account.realname,
+    		tel	:  this.account.tel,
+    		remark : this.account.remark,
+    		idcardNo : this.account.idcardNo
     	}))
     	.then( res =>{
     		console.log(res);
-    		console.log(this.$store.state.outpatientEntrance.loginRefresh())
+    		console.log(this.account)
     		// console.log(res)
     		if(res.data.codeMsg){
     			Dialog({ message: res.data.codeMsg  });
