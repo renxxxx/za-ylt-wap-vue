bcd4d16beaac8bf666732d5b8b5be6fd758e6c37
onerunsall <413038044@qq.com>
2020-03-23 15:19:28 +0800

    1

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import qs from 'qs';
 import h5p from '../build/h5p.js'
  
 debugger    
-Vue.prototype.$version = '2003230940-f0ec57f'
+Vue.prototype.$version = '2003231519-63e7132'
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
